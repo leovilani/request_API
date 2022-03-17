3795b2ff1b122f7b68d3da9df28b80fb28c18cc1
Leonardo Vilani <leonardo.v@nagro.app>
2022-03-16 22:48:04 -0300

    feat: Testing mock http request

--- src/infra/swapi_api_consumer_test.py
+++ src/infra/swapi_api_consumer_test.py
@@ -1,7 +1,10 @@
 from .swapi_api_consumer import SawapiApiConsumer
 
-def test_get_starships():
+def test_get_starships(requests_mock):
     ''' Testing get_starships method '''
+
+    requests_mock.get("https://swapi.dev/api/starships/", status_code=200, json={"some": "thing"})
+
     swapi_api_consumer = SawapiApiConsumer()
     response = swapi_api_consumer.get_starships(page=1)
 

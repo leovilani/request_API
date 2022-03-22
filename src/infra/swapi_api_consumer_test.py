5019307971483768b7eddc450793c3b6c5cc495e
Leonardo Vilani <leonardo.v@nagro.app>
2022-03-21 23:11:18 -0300

    Refactoring consumer

--- src/infra/swapi_api_consumer_test.py
+++ src/infra/swapi_api_consumer_test.py
@@ -1,11 +1,19 @@
+from typing import List
 from .swapi_api_consumer import SawapiApiConsumer
 
-def test_get_starships(requests_mock):
+def test_get_starships():
     ''' Testing get_starships method '''
 
-    requests_mock.get("https://swapi.dev/api/starships/", status_code=200, json={"some": "thing"})
+    # requests_mock.get("https://swapi.dev/api/starships/", status_code=200, json={"some": "thing"})
 
     swapi_api_consumer = SawapiApiConsumer()
-    response = swapi_api_consumer.get_starships(page=1)
+    page = 1
 
-    print(response)
+    get_starship_response = swapi_api_consumer.get_starships(page=page)
+
+    assert get_starship_response.request.method == "GET"
+    assert get_starship_response.request.url == "https://swapi.dev/api/starships/"
+    assert get_starship_response.request.params == {"page": page}
+
+    assert get_starship_response.status_code == 200
+    assert isinstance(get_starship_response.response["results"], list)
